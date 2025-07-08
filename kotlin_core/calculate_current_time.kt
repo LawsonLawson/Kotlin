9313f3cb914cc0c6219aa6a501e16a307a141680
LawsonLawson <isreallawson@outlook.com>
2025-07-08 22:54:09 +0000

    fixed a typo in comments that might be misleading ...

--- kotlin_core/calculate_current_time.kt
+++ kotlin_core/calculate_current_time.kt
@@ -10,7 +10,7 @@ fun main() {
     var remainingSeconds: Long
     val minutesInSeconds = 60 // 60 seconds make a minute
     val hoursInSeconds = minutesInSeconds * minutesInSeconds // 3600 seconds makes an hour
-    val secondsInDay = 24 * hoursInSeconds // 3600 seconds makes 24 hours (A day)
+    val secondsInDay = 24 * hoursInSeconds // 86400 seconds makes 24 hours (A day)
 
     // if we wear down the Elapsed time since 1970 (epoch) by 24 hours each time being the reset mark, we are left with remaining time till another reset
     val timeLeftSinceReset = totalSeconds % secondsInDay
