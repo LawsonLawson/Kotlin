d1048a55774d40172bedf6ea0d39f062c1de8d83
LawsonLawson <isreallawson@outlook.com>
2025-07-10 22:43:20 +0000

    hyperskill mini test

---
+++ kotlin_core/taming_exceptions.kt
@@ -0,0 +1,39 @@
+/**
+ * Write a program that calculates the division of two input numbers. It may
+ * throw an ArithmeticException. Fix it to avoid the exception.
+ * In the case when the exception can happen, the corrected program should
+ * output: Division by zero, please fix the second argument!.
+ *
+ * Sample Input 1:
+ * 8
+ * 2
+ *
+ * Sample Output 1:
+ * 4
+ *
+ *
+ * Sample Input 2:
+ * 8
+ * 0
+ *
+ * Sample Output 2:
+ * Division by zero, please fix the second argument!
+ */
+import java.util.Scanner
+
+fun computeDivision(dividend: Int, divisor: Int) {
+    try {
+        println(dividend / divisor)
+    } catch(error: ArithmeticException) {
+        println("Division by zero, please fix the second argument!")
+    }
+}
+
+fun main() {
+    val reader = Scanner(System.`in`)
+
+    val userDividend: Int = reader.nextLine().toInt()
+    val userDivisor: Int = reader.nextLine().toInt()
+
+    computeDivision(userDividend, userDivisor)
+}
