866d8ac63579f0a3fae82d9a35dd522b916fd4d4
LawsonLawson <isreallawson@outlook.com>
2025-07-22 15:03:50 +0000

    to do ..

---
+++ kotlin_core/sequence.kt
@@ -0,0 +1,19 @@
+/**
+ * Write a program that prints a part of the following sequence
+ * 1 2 2 3 3 3 4 4 4 4 5 5 5 5 5 ... (the number of repetitions is equal to the
+ * respective number). Input is a positive integer n – the number of the
+ * elements of the sequence that the program should print. Output the sequence
+ * of numbers, written in a single line, separated by spaces.
+ *
+ * For example, if n is 7, the program should output 1 2 2 3 3 3 4 (seven numbers).
+ * Note that a space at the end of the output line does not affect the result. Pay attention to the numbers.
+ *
+ * Sample Input 1:
+ * 7
+ *
+ * Sample Output 1:
+ * 1 2 2 3 3 3 4
+ */
+fun main() {
+    // TODO
+}
